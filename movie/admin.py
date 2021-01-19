2428a086f47d19c6117f1d65331631115f3b1489
Sergey <serjkasmail@gmail.com>
2021-01-19 04:27:42 -0800

    чуть переделал админку

--- movie/admin.py
+++ movie/admin.py
@@ -1,12 +1,70 @@
 from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-admin.site.register(Category),
+
+# admin.site.register(Category, CategoryAdmin),
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    # пустые формы для отзывов а дминке, ограничение
+    extra = 1
+    readonly_fields = ("name", "email",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year", 'genres')
+
+    # __name тк  нужно понять по какому полюю будем искать в модели категр
+    search_fields = ("title", "category__name")
+    # добавляем отзывы к фильму в дамине
+    inlines = [ReviewInLine]
+    # кнопки сохранения вверху
+    save_on_top = True
+    # соахранить с данными как нвоый
+    save_as = True
+    # list_editable = ("draft",)
+    # fields = (("actors", "directors", "genres"),)
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": (("year", "word_premiere", "country"),)
+        }),
+        # 'classes': ('collapse',), сокрытие группы с Имененм Actors, И название их в начале
+        ("Actors", {
+            'classes': ('collapse',),
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fees_in_world"),)
+        }),
+        ("Optional", {
+            "fields": (("url", "draft"),)
+        })
+    )
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email",)
+
+
 admin.site.register(Genre),
-admin.site.register(Movie),
+# admin.site.register(Movie),
 admin.site.register(MovieShots),
 admin.site.register(Actor),
 admin.site.register(Rating),
 admin.site.register(RatingStar),
-admin.site.register(Reviews),
-
+# admin.site.register(Reviews),
